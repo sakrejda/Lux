529a673edd037a2a0785cacd2e278b745f9bc262
Krzysztof Sakrejda-Leavitt <sakrejda@cns.umass.edu>
2012-11-21 14:28:52 -0500

    Operator precedence.

--- src/proposals_FLAT.cpp
+++ src/proposals_FLAT.cpp
@@ -112,7 +112,7 @@ Slice_td_Proposal_FLAT::~Slice_td_Proposal_FLAT() {
 arma::Col<int> Slice_td_Proposal_FLAT::propose_td() {
 	if (!theta.fresh_ll) calc_td_pdf();
 	for ( arma::uword i=0; i < theta.PHI.n_rows; ++i) {
-		td_proposed[i] = *(CH[i])(R);
+		td_proposed[i] = (*CH[i])(R);
 	}
 	return td_proposed;
 }
