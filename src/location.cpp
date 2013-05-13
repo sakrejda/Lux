41634fafe90725a54cae7d776719ebe7d910b1c7
Krzysztof Sakrejda-Leavitt <sakrejda@cns.umass.edu>
2013-05-13 10:26:08 -0400

    Added code for t_walk.

--- src/location.cpp
+++ src/location.cpp
@@ -1,5 +1,49 @@
-
 #include "location.hpp"
+#include "math.h"
+#include <boost/math/special_functions/gamma.hpp>
 
+using boost::math::lgamma;
+const double pi = boost::math::constants::pi<double>();
 
+t_walk_Posterior::t_walk_Posterior(
+		double const & x1_,
+		double 			 & x2_,
+		double const & x3_,
+		double const & p1_,   // degrees of freedom 1
+		double const & p2_,   // degrees of freedom 2
+		double const & s1_,   // scale 1
+		double const & s2_,   // scale 2
+		trng::yarn2 & R_
+) :	x1(x1_), x2(x2_), x3(x3_),
+		p1(p1_), p2(p2_), s1(s1_), s2(s2_) 
+{}
+
+std::map<std::string, double> state() const {
+	std::map<std::string, double> out;
+	out["x1"] = x1;
+	out["x2"] = x2;
+	out["x3"] = x3;
+	out["p1"] = p1;
+	out["p2"] = p2;
+	out["s1"] = s1;
+	out["s2"] = s2;
+	return out;
+}
+
+t_walk_Posterior::jump(double x2_) {
+	x2 = x2_;
+}
+// Don't divide doubles by integers, etc...
+double t_walk_Posterior::lpdf(double x2_) {
+	double lpdf;
+	lpdf = 	lgamma((p1+1.0)/2.0) - lgamma(p1/2.0) -
+		0.5 * log(p1*pi*s1^2) - 
+		(p1+1)/2 * log(1 + ((x2_-x1)^2)/(p1*s1^2) ) +
+					lgamma((p2+1)/2) - lgamma(p2/2) -
+		0.5 * log(p2*pi*s2^2) - 
+		(p2+1)/2 * log(1 + ((x3-x2_)^2)/(p2*s2^2) )
+	return lpdf;
+}
+
+t_walk_Posterior::lpdf() { return lpdf(x2); }
 
--- src/location.hpp
+++ src/location.hpp
@@ -16,17 +16,20 @@ public:
 	typedef double (*PDF)(double);
 
 	t_walk_Posterior(
-		double const & x1,
-		double 			 & x2,
-		double const & x3,
-		double const & k1,   // degrees of freedom 1
-		double const & k2,   // degrees of freedom 2
-		double const & s1,   // scale 1
-		double const & s2,   // scale 2
-		trng::yarn2 & R
+		double const & x1_,
+		double 			 & x2_,
+		double const & x3_,
+		double const & p1_,   // degrees of freedom 1
+		double const & p2_,   // degrees of freedom 2
+		double const & s1_,   // scale 1
+		double const & s2_,   // scale 2
+		trng::yarn2 & R_
 	);
+
+	std::map<std::string, double> state() const;
 	double draw();
 	void jump(double x2_);
+	void slice(double lpdf_);
 	double lpdf(double x2_);
 	double lpdf();
 
@@ -35,6 +38,10 @@ private:
 	double const & x1; 
 	double const & x2;
 	double const & x3;
+	double const & p1;
+	double const & p2;
+	double const & s1;
+	double const & s2;
 
 };
 
