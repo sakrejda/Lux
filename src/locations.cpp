1bbff644595b9a371d853dfe811de22ad759b898
Krzysztof Sakrejda-Leavitt <sakrejda@cns.umass.edu>
2013-05-22 15:30:11 -0400

    Added locations code.

---
+++ src/locations.cpp
@@ -0,0 +1,12 @@
+#include <armadillo>
+
+#include <trng/yarn2.hpp>
+#include <locations.hpp>
+
+Location::Location(arma::vec & vec, trng::yarn2 & R_) : locations(vec), R(R_) { }
+
+arma::vec Location::state() const {
+	arma::vec out = locations;
+	return out;
+}
+
--- src/locations.hpp
+++ src/locations.hpp
@@ -7,7 +7,17 @@
 #include <armadillo>
 #include <trng/yarn2.hpp>
 
-class location {
+class Location {
+
+public:
+	Location(arma::vec & vec, trng::yarn2 & R_);
+	arma::vec state() const;
+	~Location();  // delete vec, which is just a wrapper for 
+								// some R memory, but should not leak.
+
+private:
+	trng::yarn2 & R;
+	arma::vec & locations;
 
 }
 
