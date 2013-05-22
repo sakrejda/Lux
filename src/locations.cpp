309f8c648becd66d22dccb78f0b2e1c060565d74
Krzysztof Sakrejda-Leavitt <sakrejda@cns.umass.edu>
2013-05-22 16:15:13 -0400

    Consitently changed to using 'locations' rather than 'location' for names.

--- src/locations.cpp
+++ src/locations.cpp
@@ -6,7 +6,7 @@
 
 Locations::Locations(arma::vec & vec, trng::yarn2 & R_) : locations(vec), R(R_) { }
 
-arma::vec Location::state() const {
+arma::vec Locations::state() const {
 	arma::vec out = locations;
 	return out;
 }
